a3e70c6638baccdd9070b497029e3a2a3f1ce072
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-02 20:48:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => {}
 
-      }
-      :matriarch => {
-
-      }
-      :hero => {
-
-      }
-      :hero_friends => {
-
-      }
       #your key/value pairs here!
       },
     :capulet => {
